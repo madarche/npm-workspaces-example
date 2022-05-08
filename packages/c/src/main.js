f6b7a83397a61dda50ec03cac13339e87d4bba5e
Marc-Aurèle DARCHE <madarche@users.noreply.github.com>
2022-05-08 19:00:41 +0200

    Something changed in c

--- packages/c/src/main.js
+++ packages/c/src/main.js
@@ -2,9 +2,8 @@ const validator = require('validator');
 
 const a = require('@madarche/a')
 
-console.log('This is plugin c')
+console.log('This is plugin c and something changed there too')
 
 a.core()
 
 console.log(validator.isEmail('foo@bar.com'))
-
